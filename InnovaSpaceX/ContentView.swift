841ca596802f2b86842d170809339eec5e559f45
GokhanMutlu <gokhanmutlu04@yahoo.com>
2022-02-03 21:00:57 +0300

    Initial Commit

---
+++ InnovaSpaceX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InnovaSpaceX
+//
+//  Created by Gokhan Mutlu on 3.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InnovaSpaceX/InnovaSpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnovaSpaceXApp.swift
+//  InnovaSpaceX
+//
+//  Created by Gokhan Mutlu on 3.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct InnovaSpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
